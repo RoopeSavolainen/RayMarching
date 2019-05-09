332272aea6373b4583521211a20cb5cc2aa26888
Roope Savolainen <roope.savolainen@gmail.com>
2019-05-09 20:56:12 +0300

    Shader minification

--- gen_shaders.sh
+++ gen_shaders.sh
@@ -1,14 +1,16 @@
 printf "#ifndef _SHADERS_H_\n#define _SHADERS_H_\n\n" > inc/shaders.h
 
+minify="shader_minifier.exe --format none -o /dev/stdout"
+
 for f in shaders/*.frag; do
     printf "const char * %s_f = " $(basename $f .frag) >> inc/shaders.h
-    sed 's/\(.*\)/"\1\\n"/' $f >> inc/shaders.h
+    ${minify} $f | sed 's/\(.*\)/"\1\\n"/' >> inc/shaders.h
     printf ";\n" >> inc/shaders.h
 done
 
 for f in shaders/*.vert; do
     printf "const char * %s_v = " $(basename $f .vert) >> inc/shaders.h
-    sed 's/\(.*\)/"\1\\n"/' $f >> inc/shaders.h
+    ${minify} $f | sed 's/\(.*\)/"\1\\n"/' >> inc/shaders.h
     printf ";\n" >> inc/shaders.h
 done
 
